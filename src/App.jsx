a6dec0ba96a205d79dcbf1fe805954d0271f2f86
Alfredo M <kmstry214@gmail.com>
2023-06-04 21:12:27 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-// import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
+import SearchBar from './components/SearchBar/SearchBar'
 
-function App()  {
 
+
+function App() {
+
+//   useEffect(() => {
+//     const getToken = async () => {
+//         await axios.post(
+//         'https://accounts.spotify.com/api/token',
+//         new URLSearchParams({
+//             'grant_type': 'client_credentials',
+//             'client_id': 'your-client-id',
+//             'client_secret': 'your-client-secret'
+//         })
+//     );
+//     console.log(getToken)
+//     getToken()
+//   }
+// }, [])
+
+let num = 8
+console.log(num)
 
   return (
-    <>
-          
-    </>
+    <div>
+      <SearchBar />
+
+    </div>
   )
 }
 
